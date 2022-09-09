5cef39932495ed0ed4e3eeec3ac5ddf7a781e627
Bruno <bruno_moises_stz@hotmail.com>
2022-09-08 22:38:13 -0300

    Parada 08/09

--- src/main/java/com/bmoises/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/bmoises/minhasfinancas/api/resource/LancamentoResource.java
@@ -1,11 +1,21 @@
 package com.bmoises.minhasfinancas.api.resource;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bmoises.minhasfinancas.api.dto.AtualizaStatusDTO;
 import com.bmoises.minhasfinancas.api.dto.LancamentoDTO;
 import com.bmoises.minhasfinancas.exceptions.RegraNegocioException;
 import com.bmoises.minhasfinancas.model.entity.Lancamento;
@@ -15,22 +25,91 @@ import com.bmoises.minhasfinancas.model.enums.TipoLancamento;
 import com.bmoises.minhasfinancas.service.LancamentoService;
 import com.bmoises.minhasfinancas.service.UsuarioService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/lancamentos")
+@RequiredArgsConstructor
 public class LancamentoResource {
 
-	private LancamentoService service;
-	private UsuarioService usuarioService;
+	private final LancamentoService service;
+	private final UsuarioService usuarioService;
 
-	public LancamentoResource(LancamentoService service) {
-		this.service = service;
+	@GetMapping
+	public ResponseEntity buscar(@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano, @RequestParam("usuario") Long idUsuario) {
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+
+		Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);
+		if (!usuario.isPresent()) {
+			return ResponseEntity.badRequest()
+					.body("Não foi possivel realizar a consulta. Usuario não encontrado para o Id informado.");
+		} else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity salvar ( @RequestBody LancamentoDTO dto ) {
-		
+	public ResponseEntity salvar(@RequestBody LancamentoDTO dto) {
+		try {
+			Lancamento entidade = converter(dto);
+			entidade = service.salvar(entidade);
+			return new ResponseEntity(entidade, HttpStatus.CREATED);
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+
 	}
-	
+
+	@PutMapping("{id}")
+	public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDTO dto) {
+		try {
+			return service.obterPorId(id).map(entity -> {
+				Lancamento lancamento = converter(dto);
+				lancamento.setId(entity.getId());
+				service.atualizar(lancamento);
+				return ResponseEntity.ok(lancamento);
+			}).orElseGet(
+					() -> new ResponseEntity("Lancamento não encontrado na bade de Dados.", HttpStatus.BAD_REQUEST));
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@PutMapping("{id}/atualiza-status")
+	public ResponseEntity atualizarStatus(@PathVariable("id") Long id, @RequestBody AtualizaStatusDTO dto) {
+		return service.obterPorId(id).map(entity -> {
+			StatusLancamento statusSelecionado = StatusLancamento.valueOf(dto.getStatus());
+			if (statusSelecionado == null) {
+				return ResponseEntity.badRequest()
+						.body("Não foi possivel atualizar o status do lancamento, envie um status válido.");
+			} else {
+				try {
+					entity.setStatus(statusSelecionado);
+					service.atualizar(entity);
+					return ResponseEntity.ok(entity);
+				} catch (Exception e) {
+					return ResponseEntity.badRequest().body(e.getMessage());
+				}
+			}
+		}).orElseGet(() -> new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity deletar(@PathVariable("id") Long id) {
+		return service.obterPorId(id).map(entidade -> {
+			service.deletar(entidade);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}).orElseGet(() -> new ResponseEntity("Lancamento não encontrado na bade de Dados.", HttpStatus.BAD_REQUEST));
+	}
+
 	private Lancamento converter(LancamentoDTO dto) {
 		Lancamento lancamento = new Lancamento();
 		lancamento.setId(dto.getId());
@@ -38,13 +117,19 @@ public class LancamentoResource {
 		lancamento.setAno(dto.getAno());
 		lancamento.setMes(dto.getMes());
 		lancamento.setValor(dto.getValor());
-		
-		Usuario usuario = usuarioService.obterPorId(dto.getUsuario()).orElseThrow( () -> new RegraNegocioException("Usuário não encontrado para o Id informado."));
-		
+
+		Usuario usuario = usuarioService.obterPorId(dto.getUsuario())
+				.orElseThrow(() -> new RegraNegocioException("Usuário não encontrado para o Id informado."));
+
 		lancamento.setUsuario(usuario);
-		lancamento.setTipo(TipoLancamento.valueOf(dto.getTipo()));
-		lancamento.setStatus(StatusLancamento.valueOf(dto.getStatus()));
-		
+		if (dto.getTipo() != null) {
+			lancamento.setTipo(TipoLancamento.valueOf(dto.getTipo()));
+		}
+
+		if (dto.getStatus() != null) {
+			lancamento.setStatus(StatusLancamento.valueOf(dto.getStatus()));
+		}
+
 		return lancamento;
 	}
 }
--- src/main/java/com/bmoises/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/bmoises/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -3,6 +3,7 @@ package com.bmoises.minhasfinancas.service.impl;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.bmoises.minhasfinancas.exceptions.RegraNegocioException;
 import com.bmoises.minhasfinancas.model.entity.Lancamento;
 import com.bmoises.minhasfinancas.model.enums.StatusLancamento;
+import com.bmoises.minhasfinancas.model.enums.TipoLancamento;
 import com.bmoises.minhasfinancas.model.repository.LancamentoRepository;
 import com.bmoises.minhasfinancas.service.LancamentoService;
 
@@ -20,11 +22,11 @@ import com.bmoises.minhasfinancas.service.LancamentoService;
 public class LancamentoServiceImpl implements LancamentoService {
 
 	private LancamentoRepository repository;
-	
+
 	public LancamentoServiceImpl(LancamentoRepository repository) {
 		this.repository = repository;
 	}
-	
+
 	@Override
 	@Transactional
 	public Lancamento salvar(Lancamento lancamento) {
@@ -46,13 +48,14 @@ public class LancamentoServiceImpl implements LancamentoService {
 	public void deletar(Lancamento lancamento) {
 		Objects.requireNonNull(lancamento.getId());
 		repository.delete(lancamento);
-		
+
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
-		Example example = Example.of(lancamentoFiltro, ExampleMatcher.matching().withIgnoreCase().withStringMatcher(StringMatcher.CONTAINING));
+		Example example = Example.of(lancamentoFiltro,
+				ExampleMatcher.matching().withIgnoreCase().withStringMatcher(StringMatcher.CONTAINING));
 		return repository.findAll(example);
 	}
 
@@ -64,29 +67,50 @@ public class LancamentoServiceImpl implements LancamentoService {
 
 	@Override
 	public void validar(Lancamento lancamento) {
-		
-		if(lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")) {
+
+		if (lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")) {
 			throw new RegraNegocioException("Informe uma Descrição válida.");
 		}
-		
-		if(lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12) {
+
+		if (lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12) {
 			throw new RegraNegocioException("Informe um Mês válido.");
 		}
-		
-		if(lancamento.getAno() == null || lancamento.getAno().toString().length() != 4) {
+
+		if (lancamento.getAno() == null || lancamento.getAno().toString().length() != 4) {
 			throw new RegraNegocioException("Informe um Ano válido.");
 		}
-		
-		if(lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null) {
+
+		if (lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null) {
 			throw new RegraNegocioException("Informe um Usuário.");
 		}
-		
-		if(lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1) {
+
+		if (lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1) {
 			throw new RegraNegocioException("Informe um Valor válido.");
 		}
-		
-		if(lancamento.getTipo() == null) {
+
+		if (lancamento.getTipo() == null) {
 			throw new RegraNegocioException("Informe um tipe de Lancamento.");
 		}
+	}
+
+	@Override
+	public Optional<Lancamento> obterPorId(Long id) {
+		return repository.findById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public BigDecimal obterSaldoPorUsuario(Long id) {
+		BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.RECEITA);
+		BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.DESPESA);
+
+		if (receitas == null) {
+			receitas = BigDecimal.ZERO;
+		}
+
+		if (despesas == null) {
+			despesas = BigDecimal.ZERO;
+		}
+		return receitas.subtract(despesas);
 	}
 }
